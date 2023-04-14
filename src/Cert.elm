42c56b471a10cfde603cb158342135976bec859c
Sergey Mikhno <sergey.mikhno@galenica.com>
2023-04-14 14:39:45 +0200

    gets json from server

--- src/Cert.elm
+++ src/Cert.elm
@@ -6,11 +6,22 @@ import Html exposing (button, div, input, span, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Http
+import Json.Decode exposing (Decoder, bool, field, int, list, map3, map4, string, succeed)
+import Json.Decode.Pipeline exposing (optional, required)
 import Json.Encode exposing (string)
+
+
+type QuestionType
+    = Multi
+    | Single
 
 
 type alias QuestionLine =
     { text : String, selected : Bool, checked : Bool }
+
+
+type alias Answer =
+    { questionNumner : Int, letter : String, text : String, selected : Bool }
 
 
 type alias Model =
@@ -18,17 +29,61 @@ type alias Model =
 
 
 type alias Question =
-    { text : String, lines : Dict String QuestionLine }
+    { text : String, type_ : QuestionType, lines : Dict String QuestionLine }
+
+
+type alias JsonQuestion =
+    { number : Int, type_ : String, text : String, answers : List Answer }
+
+
+type alias JsonQuestions =
+    List JsonQuestion
 
 
 type Msg
-    = GotData (Result Http.Error String)
+    = GotData (Result Http.Error JsonQuestions)
     | ChangeQuestion Int
 
 
 initialSearchCmd : Cmd Msg
 initialSearchCmd =
-    Cmd.none
+    Http.get
+        { url = "http://localhost:8080/questions"
+        , expect = Http.expectJson GotData jsonQuestionsDecoder
+        }
+
+
+buildModel : Int -> Dict Int Question -> Model
+buildModel n d =
+    { current = n, questions = d }
+
+
+answerDecoder : Decoder Answer
+answerDecoder =
+    map4 Answer
+        (field "questionNumber" Json.Decode.int)
+        (field "letter" Json.Decode.string)
+        (field "text" Json.Decode.string)
+        (field "selected" bool)
+
+
+answersDecoder : Decoder (List Answer)
+answersDecoder =
+    Json.Decode.list answerDecoder
+
+
+jsonQuestionDecoder : Decoder JsonQuestion
+jsonQuestionDecoder =
+    map4 JsonQuestion
+        (field "number" Json.Decode.int)
+        (field "type" Json.Decode.string)
+        (field "text" Json.Decode.string)
+        (field "answers" answersDecoder)
+
+
+jsonQuestionsDecoder : Decoder (List JsonQuestion)
+jsonQuestionsDecoder =
+    Json.Decode.list jsonQuestionDecoder
 
 
 initialModel : Model
@@ -38,6 +93,7 @@ initialModel =
         Dict.fromList
             [ ( 1
               , { text = "This is a first question"
+                , type_ = Multi
                 , lines =
                     Dict.fromList
                         [ ( "A", { text = "This is a first question", selected = True, checked = False } )
@@ -47,6 +103,7 @@ initialModel =
               )
             , ( 2
               , { text = "This is a second question"
+                , type_ = Single
                 , lines =
                     Dict.fromList
                         [ ( "A", { text = "This is a first question", selected = True, checked = False } )
@@ -64,7 +121,7 @@ getQuestion number questions =
         question =
             case Dict.get number questions of
                 Nothing ->
-                    { text = "", lines = Dict.fromList [] }
+                    { text = "", type_ = Multi, lines = Dict.fromList [] }
 
                 Just q ->
                     q
